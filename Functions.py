62afe41e7bbe0194474014b92dfe56c1f11ad64b
SelimGüney <slmgny333@gmail.com>
2023-11-15 13:55:17 +0100

    modula

--- Functions.py
+++ Functions.py
@@ -19,9 +19,33 @@ tell_me_if_student_id_old(student)
 tell_me_if_student_id_old(student2)
 tell_me_if_student_id_old(student3)
 
+print("\n***************\n")
+
 def greeting(name):
     print("Hello", name)
 
+greeting("Ege")
+
 print("\n***************\n")
 
-greeting("Ege")
+def sum(x,y):
+    return x + y
+
+result = sum(6,7)
+
+print(result)
+
+print("\n***************\n")
+
+def is_dividable(x, y):
+    if x % y == 0:
+        return (x, "is dividable by", y)
+    else:
+        return (x, "is not dividable by", y)
+    
+print(is_dividable(10, 2))
+
+print(is_dividable(10, 3))
+
+
+
