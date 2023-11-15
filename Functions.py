db14e000b434aea30e70f033d5f002bea78fcced
SelimGüney <slmgny333@gmail.com>
2023-11-15 13:36:53 +0100

    bad way without function

---
+++ Functions.py
@@ -0,0 +1,8 @@
+student = {"name": "Selim","age":19}
+student2 = {"name": "Peter","age":20}
+student3 = {"name": "Mark","age":21}
+
+if student["age"] > 20:
+    print("Old student")
+else:
+    print("Young student")
