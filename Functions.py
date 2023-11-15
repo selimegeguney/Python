21d5fa8154e2ca1f339ce4b6d505436d4b78d39f
SelimGüney <slmgny333@gmail.com>
2023-11-15 13:43:56 +0100

    basic example

--- Functions.py
+++ Functions.py
@@ -6,7 +6,9 @@ if student["age"] > 20:
     print("Old student")
 else:
     print("Young student")
+
 print("\n***************\n")
+
 def tell_me_if_student_id_old(x):
     if x["age"] > 20:
         print("Old student")
@@ -16,3 +18,10 @@ def tell_me_if_student_id_old(x):
 tell_me_if_student_id_old(student)
 tell_me_if_student_id_old(student2)
 tell_me_if_student_id_old(student3)
+
+def greeting(name):
+    print("Hello", name)
+
+print("\n***************\n")
+
+greeting("Ege")
