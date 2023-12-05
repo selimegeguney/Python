1b1a6920bbac45d608dbf4014e657f18ab08c857
SelimGüney <slmgny333@gmail.com>
2023-12-05 12:52:08 +0100

    better optimization

--- Practice.py
+++ Practice.py
@@ -1,12 +1,24 @@
-def is_it_prime(num):
-    if num < 2 or type(num) != int:
-        return False
-    else:
-        is_prime = True
-        for i in range(2, num):
-            if num % i == 0:
-                is_prime = False
-                break
-        return is_prime
+# def is_it_prime(num):
+#     if num < 2 or type(num) != int:
+#         return False
+#     else:
+#         is_prime = True
+#         for i in range(2, num):
+#             if num % i == 0:
+#                 is_prime = False
+#                 break
+#         return is_prime
 
-print(is_it_prime(79))
+# print(is_it_prime(79))
+
+# import math
+
+# def is_it_prime(num):
+#     if num < 2 or type(num) != int:
+#         return False
+#     for i in range(2, int(math.sqrt(num)) + 1): #better for optimization
+#         if num % i == 0:
+#             return False
+#     return True
+
+# print(is_it_prime(-4.5))
